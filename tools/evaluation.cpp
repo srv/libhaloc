d7e000725bfa2f80d9f48e1f9a0e4f9d24717424
plnegre <plnegre@binibook.com>
2014-10-02 17:48:57 +0200

    Finish the implementation of openfabmap2 learning stage

--- tools/evaluation.cpp
+++ tools/evaluation.cpp
@@ -36,6 +36,11 @@ class EvaluationNode
       nhp_.param<double>("cluster_size", cluster_size_, 0.6);
       nhp_.param<double>("lower_information_bound", lower_information_bound_, 0);
       nhp_.param<int>("min_descriptor_count", min_descriptor_count_, 50);
+
+      // Output
+      nhp_.param<string>("vocab_path", vocab_path_, "vocab.yml");
+      nhp_.param<string>("cl_tree_path", cl_tree_path_, "clTree.yml");
+      nhp_.param<string>("trainbows_path", trainbows_path_, "trainbows.yml");
     }
 
     // Initialize the node
@@ -83,8 +88,10 @@ class EvaluationNode
         Mat img = imread(path, CV_LOAD_IMAGE_COLOR);
 
         ROS_INFO("[Haloc:] Detect");
+        vector<KeyPoint> kpts_;
         detector_->detect(img, kpts_);
         ROS_INFO("[Haloc:] Extract");
+        Mat descriptors_;
         extractor_->compute(img, kpts_, descriptors_);
 
         // Check if frame was useful
@@ -104,14 +111,69 @@ class EvaluationNode
 
         if ((!(train_count_ < max_images_) && max_images_ > 0))
         {
-
-          // TODO: SHUTDOWN HERE!!!!
+          shutdown();
           break;
         }
 
         // Next directory entry
         it++;
       }
+    }
+
+    // Finishes the learning stage
+    void shutdown() {
+      ROS_INFO("[Haloc:] Clustering to produce vocabulary");
+      vocab_ = trainer_.cluster();
+      ROS_INFO("[Haloc:] Vocabulary contains %d words, %d dims", vocab_.rows, vocab_.cols);
+
+      ROS_INFO("[Haloc:] Setting vocabulary...");
+      bide_->setVocabulary(vocab_);
+
+      ROS_INFO("[Haloc:] Gathering BoW's...");
+      findWords();
+
+      ROS_INFO("[Haloc:] Making the Chow Liu tree...");
+      tree_.add(bows_);
+      cl_tree_ = tree_.make(lower_information_bound_);
+
+      ROS_INFO("[Haloc:] Saving work completed...");
+      saveCodebook();
+    }
+
+    // Find words
+    void findWords()
+    {
+      Mat bow;
+      vector<KeyPoint> kpts_;
+      for (uint i=0; i<frames_sampled_.size(); i++) {
+        string path = frames_sampled_[i];
+        Mat img = imread(path, CV_LOAD_IMAGE_COLOR);
+        detector_->detect(img, kpts_);
+        bide_->compute(img, kpts_, bow);
+        bows_.push_back(bow);
+      }
+    }
+
+    // Save everything
+    void saveCodebook()
+    {
+      ROS_INFO("[Haloc:] Saving codebook...");
+      FileStorage file;
+
+      ROS_INFO_STREAM("[Haloc:] Saving Vocabulary to " << vocab_path_);
+      file.open(vocab_path_, cv::FileStorage::WRITE);
+      file << "Vocabulary" << vocab_;
+      file.release();
+
+      ROS_INFO_STREAM("[Haloc:] Saving Chow Liu Tree to " << cl_tree_path_);
+      file.open(cl_tree_path_, cv::FileStorage::WRITE);
+      file << "Tree" << cl_tree_;
+      file.release();
+
+      ROS_INFO_STREAM("[Haloc:] Saving Trained Bag of Words to " << trainbows_path_);
+      file.open(trainbows_path_, cv::FileStorage::WRITE);
+      file << "Trainbows" << bows_;
+      file.release();
     }
 
   private:
@@ -128,16 +190,21 @@ class EvaluationNode
     double sift_sigma_;
     double cluster_size_;
     double lower_information_bound_;
+    string vocab_path_;
+    string cl_tree_path_;
+    string trainbows_path_;
 
     // OpenFABMap2
     of2::FabMap *fabMap_;
+    of2::ChowLiuTree tree_;
     of2::BOWMSCTrainer trainer_;
     Ptr<FeatureDetector> detector_;
     Ptr<DescriptorExtractor>  extractor_;
     Ptr<DescriptorMatcher> matcher_;
     Ptr<BOWImgDescriptorExtractor> bide_;
-    vector<KeyPoint> kpts_;
-    Mat descriptors_;
+    Mat vocab_;
+    Mat bows_;
+    Mat cl_tree_;
     vector<string> frames_sampled_;
 };
 
